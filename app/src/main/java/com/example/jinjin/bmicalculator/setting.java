d59e3d7a761044a18b3988257c592d464fd08845
Hz46100 <99510072+Hz46100@users.noreply.github.com>
2022-07-17 23:01:22 +0800

    更新设置界面的背景判断

--- app/src/main/java/com/example/jinjin/bmicalculator/setting.java
+++ app/src/main/java/com/example/jinjin/bmicalculator/setting.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class setting extends AppCompatActivity {
-Button btn_back;
+    Button btn_back;
+    int count = 0;
+    private static final String TAG = "setting";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,18 +22,19 @@ Button btn_back;
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //把activity背景变成黑色
-                getWindow().getDecorView().setBackgroundResource(R.color.black);
-                //再次点击按钮恢复原来的颜色
-                btn_back.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        getWindow().getDecorView().setBackgroundResource(R.color.white);
-                    }
-                });
+                count++;
+                //点击按钮判断count是否为单数，如果是，则变黑色，否则恢复白色
+                if (count % 2 == 1) {
+                    //把activity背景变成黑色
+                    getWindow().getDecorView().setBackgroundResource(R.drawable.background);
+                } else if (count % 2 == 0) {
+                    //再次点击按钮恢复原来的颜色
+                    getWindow().getDecorView().setBackgroundResource(R.color.white);
+                }
             }
         });
     }
+
     //删除系统默认的标题栏
     //onWindowFocusChanged方法是在Activity的onCreate方法执行完毕后才执行的，所以在这里我们可以使用，
     // 因为在onCreate方法中我们已经设置了一个标题栏，所以在这里我们可以删除系统默认的标题栏。
@@ -36,7 +42,7 @@ Button btn_back;
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         if (hasFocus) {
-           NoBar();
+            NoBar();
         }
     }
 
